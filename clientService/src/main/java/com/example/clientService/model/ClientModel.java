2cb7c47ed4d6ff334c6cf988416d60295dd913b9
xXjjstillXxgit config --global user.name xXjjstillXxgit config --global user.name xXjjstillXx <xxjjatillxx@gmail.com>
2023-11-21 17:59:44 -0500

    Plantilla

--- clientService/src/main/java/com/example/clientService/model/ClientModel.java
+++ clientService/src/main/java/com/example/clientService/model/ClientModel.java
@@ -1,5 +1,10 @@
 package com.example.clientService.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class ClientModel {
     
 }
